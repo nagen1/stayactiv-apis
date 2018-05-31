648b5ed4d46e8d587e8c79e6ed2e3970bc2c3c1a
nagen-mac <rao.jogi@gmail.com>
2018-05-30 22:19:48 -0500

    jsonify changes

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
+# https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
 @app.route('/')
 def hello_world():
-  return 'Hello, Welcome to StayActiv!'
+  return '<H1>Hello, Welcome to StayActiv!</H1>'
+
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    #return "Welcome"
+    return jsonify({'tasks': tasks})
+
 
 if __name__ == '__main__':
   app.run()
